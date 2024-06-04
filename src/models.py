976c9973fa6c0e82ed191ab5d8b269dae411b3b1
TonyGlide <162181740+TonyGlide@users.noreply.github.com>
2024-06-04 00:54:10 +0000

    p1

--- src/models.py
+++ src/models.py
@@ -7,23 +7,60 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    fisrt_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    phone = Column(String(250))
+    gender = Column(String(250))
+    
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    description = Column(String(250))
+    icons = Column(String(250), nullable=False)
+    users_id = Column(Integer, ForeignKey('users.id'))
+    users = relationship(User)
+
+class Comment(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    comments = Column(String(250))
+    users_id = Column(Integer, ForeignKey('users.id'))
+    posts_id = Column(Integer, ForeignKey('posts.id'))
+    users = relationship(User)
+    posts = relationship(Post)
+    
+class Reaction(Base):
+    __tablename__ = 'reactions'
+    id = Column(Integer, primary_key=True)
+    number_of_likes = Column(String(250))
+    number_of_dislikes = Column(String(250))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    comment_id = Column(Integer, ForeignKey('comments.id'))
+    posts = relationship(Post)
+    comments = relationship(Comment)
+
+class Media(Base):
+    __tablename__ = 'medias'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(250))
+    url = Column(String(250))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    posts = relationship(Post)
+    
+class Follower(Base):
+    __tablename__ = 'followers'
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('users.id'))
+    user_to_id = Column(Integer, ForeignKey('users.id'))
+    users_id = relationship(User)
+    
 
     def to_dict(self):
         return {}
